cdbc2c5c1ef3ddcab2d4543f25a8e1c594d5129b
crasher746 <Crasher746@gmail.com>
2018-03-14 23:28:46 +0700

    Data output in the boxes

--- main.c
+++ main.c
@@ -62,7 +62,7 @@ int main() {
 				mt_setfgColor(WHITE);
 				printf("\nFLAGS: OVERFLOW: %d OUTOFMEMORY: %d\n", memoryFlag, commandFlag);
 				printf(">BEFORE ENCODE / DECODE< COMMAND: %d OPERAND: %d VALUE: %d\n", command, operand, result);
-				command = 61; operand = 3;
+				command = 61; operand = 3;/////!!!!!//////
 				printf(">DATA INPUT< COMMAND: %d OPERAND: %d\n", command, operand);
 				mt_setbgColor(WHITE);
 				mt_setfgColor(PURPLE);
@@ -97,7 +97,7 @@ int main() {
 				printf("\n\n");
 				for (i = 0; i < N_MEM; i++) {
 					mt_setfgColor(PURPLE);
-					printf("+%X ", arrMemory[i]);
+					printf("+%04X ", arrMemory[i]);
 					if (count % 10 == 0) printf("\n");
 					count++;
 				}
@@ -115,7 +115,10 @@ int main() {
 				printf("MEMORY");
 				mt_gotoXY(2,2);
 				for (i = 1; i <= N_MEM; i++) {
-					printf("+%X ", arrMemory[i - 1]);
+					if (arrMemory[i - 1] != 0) {mt_setbgColor(DARK_BLUE); printf("+%04X", arrMemory[i - 1]); mt_setbgColor(WHITE); printf(" ");}
+					else {
+						printf("+%04X ", arrMemory[i - 1]);
+					}
 					if (i % 10 == 0 && i != 0) {k++; mt_gotoXY(k, 2);}				
 				}
 
@@ -125,6 +128,37 @@ int main() {
 				BIG[0] = 0;
 				BIG[1] = 0;
 				char buffer[20];
+				
+				/*bc_setBigCharPos(BIG, 0, 1, 1);
+				bc_setBigCharPos(BIG, 0, 2, 1);
+				bc_setBigCharPos(BIG, 0, 3, 1);
+				bc_setBigCharPos(BIG, 0, 4, 1);
+				bc_setBigCharPos(BIG, 0, 5, 1);
+				bc_setBigCharPos(BIG, 0, 6, 1);
+				bc_setBigCharPos(BIG, 1, 1, 1);
+				bc_setBigCharPos(BIG, 1, 6, 1);
+				bc_setBigCharPos(BIG, 2, 1, 1);
+				bc_setBigCharPos(BIG, 2, 6, 1);
+				bc_setBigCharPos(BIG, 3, 1, 1);
+				bc_setBigCharPos(BIG, 3, 6, 1);
+				bc_setBigCharPos(BIG, 4, 1, 1);
+				bc_setBigCharPos(BIG, 4, 6, 1);
+				bc_setBigCharPos(BIG, 5, 1, 1);
+				bc_setBigCharPos(BIG, 5, 6, 1);
+				bc_setBigCharPos(BIG, 6, 1, 1);
+				bc_setBigCharPos(BIG, 6, 6, 1);
+				bc_setBigCharPos(BIG, 7, 1, 1);
+				bc_setBigCharPos(BIG, 7, 2, 1);
+				bc_setBigCharPos(BIG, 7, 3, 1);
+				bc_setBigCharPos(BIG, 7, 4, 1);
+				bc_setBigCharPos(BIG, 7, 5, 1);
+				bc_setBigCharPos(BIG, 7, 6, 1);			
+				mt_gotoXY(16, 48);
+				printf(" %d %d", BIG[0], BIG[1]);			
+				bc_printBigChar(BIG, 15, y, PURPLE, WHITE);*/
+
+				//chooseBig(BIG, 'F');
+				//bc_printBigChar(BIG, 15, y, PURPLE, WHITE);
 				sprintf(buffer, "+%04X", result);
 				for (i = 0; i < 5; i++) {
 					chooseBig(BIG, buffer[i]);
@@ -138,22 +172,39 @@ int main() {
 				bc_box(1, 63, 3, 18);
 				mt_gotoXY(1, 67);
 				printf("ACCUMULATOR");
+				mt_gotoXY(2, 69);
+				printf("+%04X", result);
 
 				bc_box (4, 63, 3, 18);
 				mt_gotoXY(4, 69);
 				printf("COUNTER");
+				mt_gotoXY(5, 69);
+				printf("+0000");
 
 				bc_box (7, 63, 3, 18);
 				mt_gotoXY(7, 68);
 				printf("OPERATION");
+				mt_gotoXY(8, 71);
+				printf("%d", command);
 
 				bc_box (10, 63, 3, 18);
 				mt_gotoXY(10, 70);
 				printf("FLAGS");
-
+				mt_gotoXY(14, 47);
+				sc_regGet(OVERFLOW, &memoryFlag);
+				mt_gotoXY(15, 47);
+				sc_regGet(WRONGCOMMAND, &commandFlag);
+				mt_gotoXY(11, 64);
+				if ((memoryFlag == 1) || (commandFlag == 1)) {
+					mt_setfgColor(DARK_BLUE);					
+					printf("OVFLW: %d CMND: %d", memoryFlag, commandFlag);
+					mt_setfgColor(PURPLE);
+				}
+				else {mt_gotoXY(11, 71); printf("0");}
 				bc_box (13, 47, 18, 34);
 				mt_gotoXY(13, 62);
 				printf("KEYS");
+				mt_gotoXY(16, 48);
 				scanf("%d", &helpme);
 
 			break;
